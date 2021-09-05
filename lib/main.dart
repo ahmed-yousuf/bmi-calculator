028d11bc760fd771641d29eda7ad050fee7ed459
ahmedyousuf <hammoody071@gmail.com>
2021-09-05 16:34:38 +0300

    init push for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,66 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'constants.dart';
+import 'onboarding.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:bmi_calculator/screens/input_page.dart';
+import 'screens/input_page.dart';
 
-void main() => runApp(BMICalculator());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en', 'US'), Locale('ar', 'SA')],
+      path: 'assets/translations',
+      //saveLocale: true,
+      fallbackLocale: Locale('en','US'),
+      child: BMICalculator(),
+    )
+  );
+}
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
+      title: 'BMI Calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: bgprimaryColor,
+        scaffoldBackgroundColor:  Color(0xFF0A0E21),
+      ),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: SplashScreen(),
     );
   }
 }
 
-class InputPage extends StatefulWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  _InputPageState createState() => _InputPageState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _InputPageState extends State<InputPage> {
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds:5), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => OnBoardingPage()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
+      backgroundColor: bgprimaryColor,
       body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: Lottie.asset('assets/intro.json'),
       ),
     );
   }
 }
+
